7af99e312d0cd1569c04d6db1fe61a53ea56f61b
costolo <ben.costolo@gmail.com>
2017-11-11 12:37:02 -0500

    2 0 1 7 u p i n h e r e

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+    ],
+  },
+};
